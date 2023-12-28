fc539ce3617f3f7a993aecf64baf97f88a32be25
carey <c.chomsoonthorn@arts.ac.uk>
2023-12-28 10:07:19 +0000

    fixed some stuff maybe update 28/12/23 10:06am

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonosctcp",
-    version="0.1",
+    version="1",
     packages=find_packages(),
     install_requires=[],
     package_data={
