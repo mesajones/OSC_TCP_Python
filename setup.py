aa85a3ebaafef5398c0e8df80851e282466c61c7
carey home pc <carey.chomsoonthorn@gmail.com>
2024-08-29 20:55:24 +0100

    29/08/24 8:54PM updated the dispatcher class to allow for multiple handlers.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonosctcp",
-    version="1",
+    version="2.0",
     packages=find_packages(),
     install_requires=[],
     package_data={
